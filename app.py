2aa3ac95294f3af96ada13cf01534eb095ee8404
Ardilla Firosya <135436843+Ardilla-Firosya@users.noreply.github.com>
2024-05-07 14:04:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -46,4 +46,31 @@ st.pyplot()
 
 # Note: Make sure to have the 'tips.csv' file in the same directory as this script
 
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load data
+data = pd.read_csv("tips.csv")
+
+# Streamlit app
+st.title("Tips Analysis")
+
+# Display the dataframe
+st.write("## Tips Data")
+st.write(data)
+
+# Line plot
+st.write("## Line Plot")
+plt.plot(data['tip'], label='Tip')
+plt.plot(data['size'], label='Size')
+plt.title("Line Plot")
+plt.xlabel('Day')
+plt.ylabel('Value')
+plt.legend()
+st.pyplot()
+
+# Note: Make sure to have the 'tips.csv' file in the same directory as this script
+
+
 
