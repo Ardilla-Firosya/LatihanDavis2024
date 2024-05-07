3220788a5d5b3a7f56366737c458fa192d9d267c
Ardilla Firosya <135436843+Ardilla-Firosya@users.noreply.github.com>
2024-05-07 13:32:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 import streamlit as st
 import pandas as pd
+import seaborn as pd
 import matplotlib.pyplot as plt
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 data = pd.read_csv("tips.csv")
 
 st.subheader("Visualisasi Dashboard")
+
+# Bar chart with day against tip
+plt.bar(data['day'], data['tip'])
+
+# plt.title("Bar Chart")
+
+# Setting the X and Y labels
+plt.xlabel('Day')
+plt.ylabel('Tip')
+
+# Show plot
+st.pyplot()
+
+
 plt.scatter(data['day'], data['tip'], c=data['size'],
             s=data['total_bill'])
             
