56969448bb48e767785cef9a4b280a95e547c6b2
Ardilla Firosya <135436843+Ardilla-Firosya@users.noreply.github.com>
2024-05-07 13:57:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -56,3 +56,28 @@ st.pyplot()
 
 # Note: Make sure to have the 'tips.csv' file in the same directory as this script
 
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load data
+data = pd.read_csv("tips.csv")
+
+# Streamlit app
+st.title("Tips Analysis")
+
+# Display the dataframe
+st.write("## Tips Data")
+st.write(data)
+
+# Scatter plot
+st.write("## Scatter Plot")
+plt.scatter(data['day'], data['tip'])
+plt.title("Scatter Plot")
+plt.xlabel('Day')
+plt.ylabel('Tip')
+st.pyplot()
+
+# Note: Make sure to have the 'tips.csv' file in the same directory as this script
+
+
