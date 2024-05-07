7bbc56dcf1c4b03253e901d357a5ee345ab099f4
Ardilla Firosya <135436843+Ardilla-Firosya@users.noreply.github.com>
2024-05-07 12:58:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
+import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+st.set_option('deprecation.showPyplotGlobalUse', False)
 data = pd.read_csv("tips.csv")
 
+st.subheader("Visualisasi Dashboard")
+plt.scatter(data['day'], data['tip'], c=data['size'],
+            s=data["total_bill'])
+            
 plt.plot(data['tip'])
 plt.plot(data['size'])
 
